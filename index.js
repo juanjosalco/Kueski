7640b9ced632e2953f770dd031ab57663ebe696a
Juan Salazar <jjsc2003@live.com>
2023-04-21 15:40:09 -0600

    Added Express

---
+++ index.js
@@ -0,0 +1,10 @@
+// server/index.js
+const express = require("express");
+
+const app = express();
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
