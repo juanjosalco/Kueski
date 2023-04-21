758dfcd5486fd522a3ec7e7237984b05d37d9f32
Juan Salazar <jjsc2003@live.com>
2023-04-21 15:56:03 -0600

    Added first endpoint

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 // server/index.js
 const express = require("express");
 
-const app = express();
+require('dotenv').config()
+const mysql = require('mysql2')
+
+const DB=process.env.DATABASE_URL
+const connection= mysql.createConnection(DB)
+
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
 
 const PORT = process.env.PORT || 4000;
+
+const app = express();
+
+app.get("/api/users", (req, res) => {
+    var query="SELECT * FROM USERS";
+    connection.query(query, function (err, result, fields) {
+      if (err) throw err;
+      res.send(result)
+    }
+    );
+  });
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
