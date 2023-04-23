d86ccff6e4155db1506cb3cc60e8828978944ff8
Juan Salazar <jjsc2003@live.com>
2023-04-23 15:53:57 -0600

    Added new Styles and Merged With Branch 3

--- arco-system/src/Views/Dashboard.jsx
+++ arco-system/src/Views/Dashboard.jsx
@@ -2,27 +2,50 @@ import React from "react";
 import Table from "../Components/Table";
 
 import { makeStyles } from "@mui/styles";
+<<<<<<< HEAD
+=======
+import { AppBar, Toolbar, Button, Typography, Container } from "@mui/material";
+>>>>>>> 7ecca6c (Added new Styles and Merged With Branch 3)
 import { Box } from "@mui/material";
 
 const useStyles = makeStyles((theme) => ({
   container: {
-    padding: 80,
+    margin: 80,
+    border: '3px solid black',
+    borderBottom: '0px solid black'
   },
-  TableContatiner:{
-    borderTop: '4px solid black',
-    borderLeft: '4px solid black',
-  }
 }));
 
 function Dashboard() {
     const classes = useStyles()
   return (
-
-    <Box className={classes.container}>
-       <Box className={classes.TableContatiner}>
-          <Table/>
-       </Box>
-    </Box>
+    <Container maxWidth='xl' style={{minWidth: '1550px'}}>
+      <AppBar sx={{ backgroundColor: "#2A3852" }}>
+        <Toolbar>
+          <Typography>Arco System</Typography>
+          <Button
+            sx={{ marginLeft: "auto" }}
+            variant="contained"
+            onClick={() => handleView("Users")}
+          >
+            Users{" "}
+          </Button>
+          <Button
+            sx={{ marginLeft: "10px" }}
+            variant="contained"
+            onClick={() => handleView("Logs")}
+          >
+            Logs{" "}
+          </Button>
+        </Toolbar>
+      </AppBar>
+      {view === "Users" && (
+        <Box className={classes.container}>
+          <Table />
+        </Box>
+      )}
+      {view === "Logs" && <Box className={classes.container}></Box>}
+    </Container>
   );
 }
 
