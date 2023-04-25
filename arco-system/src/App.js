95a49f911a3bcd2c95bf5635e9b7cc06b906ec3a
NolbertoCastro <nolber0703@gmail.com>
2023-04-25 09:59:34 -0600

    oposition ready

--- arco-system/src/App.js
+++ arco-system/src/App.js
@@ -2,10 +2,12 @@ import React from "react";
 import "./App.css";
 import Dashboard from "./Views/Dashboard";
 import Header from "./Components/Header";
+import Oposicion from "./Components/Oposicion.jsx";
 
 function App() {
   return (
     <>
+      {/* <Oposicion/> */}
       <Header/>
       <Dashboard/>
     </>
