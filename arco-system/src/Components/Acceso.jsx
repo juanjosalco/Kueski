416f899bd127bcbe415c2f64315cba0086050b79
Juan Salazar <jjsc2003@live.com>
2023-05-02 17:41:42 -0600

    Data correctly fetched

--- arco-system/src/Components/Acceso.jsx
+++ arco-system/src/Components/Acceso.jsx
@@ -54,19 +54,11 @@ const useStyles = makeStyles((theme) => ({
     }
 }));
 
-function Acceso({isOpen, handleClose, id}) {
+function Acceso({isOpen, handleClose, user}) {
     const classes = useStyles();
-    const [data, setData] = useState([]);
 
-    useEffect(() => {
-        fetch(`/api/users/${id}`)
-          .then((res) => res.json())
-          .then((data) => setData(data))
-          .catch((err) => console.log(err));
-      }, [id]);
-    console.log(data); console.log(id);
     return (
-    <Modal open={isOpen} onClose={handleClose} style={{overflow: "scroll"}}>
+        <Modal open={isOpen} onClose={handleClose} style={{overflow: "scroll"}}>
         <div className={classes.blackBack}>
             <section style={{display: "flex", flexDirection: "column", width:"80%", padding: "64px", backgroundColor: "#ffffff", gap: "48px"}}>
                 <div style={{display: "flex", justifyContent: "space-between", alignItems: "center"}}>
@@ -76,106 +68,104 @@ function Acceso({isOpen, handleClose, id}) {
                     </button>
                 </div>
                 <section style={{display: "flex", width: "100%", justifyContent: "space-between", gap: "32px"}}>
-                  (data ?   <section className={classes.dataSection}>
+                   <section className={classes.dataSection}>
                         <h1 className={classes.newSectionTitle}>Información del usuario</h1>
                         <hr className={classes.divisor}/>
                         <section className={classes.columns}>
                             <section className={classes.side}>
                                 <section className={classes.dataSection}>
                                     <h1 className={classes.dataTitle}>Nombre</h1>
-                                    <p className={classes.data}>{data[0].F_NAME}</p>
+                                    <p className={classes.data}>{user.row.F_NAME}</p>
                                 </section>
                                 <section className={classes.dataSection}>
                                     <h1 className={classes.dataTitle}>Fecha de Nacimiento</h1>
-                                    <p className={classes.data}>{data[0].BIRTH_DATE.substring(0,10) }</p>
+                                    <p className={classes.data}>{user.row.BIRTH_DATE.substring(0,10) }</p>
                                 </section>
                                 <section className={classes.dataSection}>
                                     <h1 className={classes.dataTitle}>Estado de Nacimiento</h1>
-                                    <p className={classes.data}>{data[0].STATE_BORN_IN}</p>
+                                    <p className={classes.data}>{user.row.STATE_BORN_IN}</p>
                                 </section>
                                 <section className={classes.dataSection}>
                                     <h1 className={classes.dataTitle}>RFC</h1>
-                                    <p className={classes.data}>{data[0].RFC}</p>
+                                    <p className={classes.data}>{user.row.RFC}</p>
                                 </section>
                             </section>
                             <section className={classes.side}>
                                 <section className={classes.dataSection}>
                                     <h1 className={classes.dataTitle}>Primer Apellido</h1>
-                                    <p className={classes.data}>{data[0].LNAME1}</p>
+                                    <p className={classes.data}>{user.row.LNAME1}</p>
                                 </section>
                                 <section className={classes.dataSection}>
                                     <h1 className={classes.dataTitle}>Nacionalidad</h1>
-                                    <p className={classes.data}>{data[0].NATIONALITY}</p>
+                                    <p className={classes.data}>{user.row.NATIONALITY}</p>
                                 </section>
                                 <section className={classes.dataSection}>
                                     <h1 className={classes.dataTitle}>Actividad Económica</h1>
-                                    <p className={classes.data}>{data[0].OCCUPATION}</p>
+                                    <p className={classes.data}>{user.row.OCCUPATION}</p>
                                 </section>
                                 <section className={classes.dataSection}>
                                     <h1 className={classes.dataTitle}>Número de Telefono</h1>
-                                    <p className={classes.data}>{data[0].PHONE_NUMBER}</p>
+                                    <p className={classes.data}>{user.row.PHONE_NUMBER}</p>
                                 </section>
                             </section>
                             <section className={classes.side}>
                                 <section className={classes.dataSection}>
                                     <h1 className={classes.dataTitle}>Segundo Apellido</h1>
-                                    <p className={classes.data}>{data[0].LNAME2}</p>
+                                    <p className={classes.data}>{user.row.LNAME2}</p>
                                 </section>
                                 <section className={classes.dataSection}>
                                     <h1 className={classes.dataTitle}>Género</h1>
-                                    <p className={classes.data}>{data[0].GENDER}</p>
+                                    <p className={classes.data}>{user.row.GENDER}</p>
                                 </section>
                                 <section className={classes.dataSection}>
                                     <h1 className={classes.dataTitle}>CURP</h1>
-                                    <p className={classes.data}>{data[0].CURP}</p>
+                                    <p className={classes.data}>{user.row.CURP}</p>
                                 </section>
                                 <section className={classes.dataSection}>
                                     <h1 className={classes.dataTitle}>Email</h1>
-                                    <p className={classes.data}>{data[0].EMAIL}</p>
+                                    <p className={classes.data}>{user.row.EMAIL}</p>
                                 </section>
                             </section>
                         </section>
-
                         <h1 className={classes.newSectionTitle}>Direccción del usuario</h1>
                         <hr className={classes.divisor}/>
                         <section className={classes.columns}>
                             <section className={classes.side}>
                                 <section className={classes.dataSection}>
                                     <h1 className={classes.dataTitle}>País</h1>
-                                    <p className={classes.data}>{data[0].COUNTRY}</p>
+                                    <p className={classes.data}>{user.row.COUNTRY}</p>
                                 </section>
                                 <section className={classes.dataSection}>
                                     <h1 className={classes.dataTitle}>Colonia</h1>
-                                    <p className={classes.data}>{data[0].NEIGHBORHOOD}</p>
+                                    <p className={classes.data}>{user.row.NEIGHBORHOOD}</p>
                                 </section>
                                 <section className={classes.dataSection}>
                                     <h1 className={classes.dataTitle}>Número Exterior</h1>
-                                    <p className={classes.data}>{data[0].EXT_NUMBER}</p>
+                                    <p className={classes.data}>{user.row.EXT_NUMBER}</p>
                                 </section>
-
                             </section>
                             <section className={classes.side}>
                                 <section className={classes.dataSection}>
                                     <h1 className={classes.dataTitle}>Estado</h1>
-                                    <p className={classes.data}>{data[0].STATE}</p>
+                                    <p className={classes.data}>{user.row.STATE}</p>
                                 </section>
                                 <section className={classes.dataSection}>
                                     <h1 className={classes.dataTitle}>Código Postal</h1>
-                                    <p className={classes.data}>{data[0].ZIP_CODE}</p>
+                                    <p className={classes.data}>{user.row.ZIP_CODE}</p>
                                 </section>
                                 <section className={classes.dataSection}>
                                     <h1 className={classes.dataTitle}>Número Interior</h1>
-                                    <p className={classes.data}>{data[0].INT_NUMBER}</p>
+                                    <p className={classes.data}>{user.row.INT_NUMBER}</p>
                                 </section>
                             </section>
                             <section className={classes.side}>
                                 <section className={classes.dataSection}>
                                     <h1 className={classes.dataTitle}>Ciudad</h1>
-                                    <p className={classes.data}>{data[0].CITY}</p>
+                                    <p className={classes.data}>{user.row.CITY}</p>
                                 </section>
                                 <section className={classes.dataSection}>
                                     <h1 className={classes.dataTitle}>Calle</h1>
-                                    <p className={classes.data}>{data[0].STREET}</p>
+                                    <p className={classes.data}>{user.row.STREET}</p>
                                 </section>
                             </section>
                         </section>
@@ -185,18 +175,17 @@ function Acceso({isOpen, handleClose, id}) {
                             <section className={classes.side2}>
                                 <section className={classes.dataSection}>
                                     <h1 className={classes.dataTitle}>Tipo de Identificación</h1>
-                                    <p className={classes.data}>{data[0].ID_NUMBER}</p>
+                                    <p className={classes.data}>{user.row.ID_NUMBER}</p>
                                 </section>
                             </section>
                             <section className={classes.side2}>
                                 <section className={classes.dataSection}>
                                     <h1 className={classes.dataTitle}>Número de Identificación</h1>
-                                    <p className={classes.data}>{data[0].ID_TYPE}</p>
+                                    <p className={classes.data}>{user.row.ID_TYPE}</p>
                                 </section>
                             </section>
                         </section>
-
-                    </section> : <p>Loading </p> );
+                    </section>
                 </section>
                 <button style={{maxWidth: "200px", backgroundColor: "#48cd00", fontWeight: "700", color: "#ffffff", fontSize:"18px", padding: "8px", alignSelf: "flex-end"}}>Generar Reporte</button>
             </section>
