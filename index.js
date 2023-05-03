6b1a0e04714c685a9b7699692ec60af0194ee899
Juan Salazar <jjsc2003@live.com>
2023-05-03 12:09:50 -0600

    deploy

--- index.js
+++ index.js
@@ -19,30 +19,6 @@ const path = require("path");
 
 app.use(bodyParser.json());
 
-const allowCors = fn => async (req, res) => {
-  res.setHeader('Access-Control-Allow-Credentials', true)
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  // another common pattern
-  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  )
-  if (req.method === 'OPTIONS') {
-    res.status(200).end()
-    return
-  }
-  return await fn(req, res)
-}
-
-const handler = (req, res) => {
-  const d = new Date()
-  res.end(d.toString())
-}
-
-allowCors(handler)
-
 app.get("/api/users", (req, res) => {
   connection.connect(function (err) {
     if (err) throw err;
