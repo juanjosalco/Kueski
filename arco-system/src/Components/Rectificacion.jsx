f5512de2e029cffdd39a7c9517300f66e89149e4
Juan Salazar <jjsc2003@live.com>
2023-05-03 15:22:12 -0600

    deploy

--- arco-system/src/Components/Rectificacion.jsx
+++ arco-system/src/Components/Rectificacion.jsx
@@ -152,7 +152,7 @@ function Rectificacion({ isOpen, handleClose , id}) {
   });
   console.log(body);
    
-  fetch(`https://kueski.vercel.app//users/${id}`,{
+  fetch(`https://kueski.vercel.app/users/${id}`,{
     method: 'PATCH',
     mode: 'no-cors',
     headers: {
