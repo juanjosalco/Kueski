60ff8cd0d2ed6cb8aa614598d8a359903c4e5d3b
Mar Bustos <marianabhdez@gmail.com>
2023-05-03 00:58:43 -0600

    cambios en estilos de pdf

--- arco-system/src/Components/AccesoPDF.jsx
+++ arco-system/src/Components/AccesoPDF.jsx
@@ -21,19 +21,25 @@ const styles = StyleSheet.create({
           fontWeight: 'bold',
           marginRight: 10,
           marginBottom: 5,
+          color: '#333',
           fontSize: 12,
         },
         data: {
           fontSize: 12,
           marginBottom: 10,
+          color: '#333',
         },
-        container: {
-            display: 'flex',
+        row: {
             flexDirection: 'row',
-            '@media max-width: 400': {
-                flexDirection: 'column',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            marginBottom: 20,
+        },
+        column: {
+            flex: 1,
+            flexDirection: 'column',
+            paddingHorizontal: 10,
             
-            }
         },
   });
   
@@ -49,61 +55,69 @@ function AccesoPDF({user}) {
                     <View style={styles.section}>
                         <Text style={{ fontSize: 18, marginBottom: 10 }}>Información del usuario</Text>
 
-                        <View styles={styles.container}>
-                            <View style={{ flexDirection: "column"}}>
+                        <View style={styles.row}>
+                            <View style={styles.column}>
                                 <Text style={styles.dataTitle}>Nombre:</Text>
                                 <Text style={styles.data}>{user.F_NAME}</Text>
                             </View>
-
-                            <View style={{ flexDirection: 'column' }}>
-                                <Text style={styles.dataTitle}>Fecha de Nacimiento:</Text>
-                                <Text style={styles.data}>{user.BIRTH_DATE.substring(0,10)}</Text>
-                            </View>
-
-                            <View style={{ flexDirection: 'column' }}>
-                                <Text style={styles.dataTitle}>Estado de Nacimiento:</Text>
-                                <Text style={styles.data}>{user.STATE_BORN_IN}</Text>
-                            </View>
-                        </View>
-                            <View style={{ flexDirection: 'column' }}>
-                                <Text style={styles.dataTitle}>RFC:</Text>
-                                <Text style={styles.data}>{user.RFC}</Text>
-                            </View>
-
-                            <View style={{ flexDirection: 'column' }}>
+                            <View style={styles.column}>
                                 <Text style={styles.dataTitle}>Primer Apellido:</Text>
                                 <Text style={styles.data}>{user.LNAME1}</Text>
                             </View>
-
-                            <View style={{ flexDirection: 'column' }}>
+                            <View style={styles.column}>
+                                <Text style={styles.dataTitle}>Segundo Apellido:</Text>
+                                <Text style={styles.data}>{user.LNAME2}</Text>
+                            </View>
+                        </View>
+                        <View style={styles.row}>
+                        <View style={styles.column}>
+                                <Text style={styles.dataTitle}>Fecha de Nacimiento:</Text>
+                                <Text style={styles.data}>{user.BIRTH_DATE.substring(0,10)}</Text>
+                            </View>
+                            <View style={styles.column}>
                                 <Text style={styles.dataTitle}>Nacionalidad:</Text>
                                 <Text style={styles.data}>{user.NATIONALITY}</Text>
                             </View>
-
-                            <View style={{ flexDirection: 'column' }}>
+                            
+                            <View style={styles.column}>
+                                <Text style={styles.dataTitle}>Estado de Nacimiento:</Text>
+                                <Text style={styles.data}>{user.STATE_BORN_IN}</Text>
+                            </View>
+                            
+                        </View>
+                        <View style={styles.row}>
+                            <View style={styles.column}>
+                                <Text style={styles.dataTitle}>Género:</Text>
+                                <Text style={styles.data}>{user.GENDER}</Text>
+                            </View>
+                            <View style={styles.column}>
                                 <Text style={styles.dataTitle}>Actividad Económica:</Text>
                                 <Text style={styles.data}>{user.OCCUPATION}</Text>
                             </View>
 
-                            <View style={{ flexDirection: 'column' }}>
+                            <View style={styles.column}>
+                                <Text style={styles.dataTitle}>RFC:</Text>
+                                <Text style={styles.data}>{user.RFC}</Text>
+                            </View>
+                            
+                        </View>
+                        <View style={styles.row}>
+                            
+                            <View style={styles.column}>
+                                <Text style={styles.dataTitle}>CURP:</Text>
+                                <Text style={styles.data}>{user.CURP}</Text>
+                            </View>
+
+                            <View style={styles.column}>
                                 <Text style={styles.dataTitle}>Número de Telefono:</Text>
                                 <Text style={styles.data}>{user.PHONE_NUMBER}</Text>
                             </View>
 
-                            <View style={{ flexDirection: 'column' }}>
-                                <Text style={styles.dataTitle}>Segundo Apellido:</Text>
-                                <Text style={styles.data}>{user.LNAME2}</Text>
+                            <View style={styles.column}>
+                                <Text style={styles.dataTitle}>Correo electrónico:</Text>
+                                <Text style={styles.data}>{user.EMAIL}</Text>
                             </View>
-
-                            <View style={{ flexDirection: 'column' }}>
-                                <Text style={styles.dataTitle}>Género:</Text>
-                                <Text style={styles.data}>{user.GENDER}</Text>
-                            </View>
-
-                            <View style={{ flexDirection: 'column' }}>
-                                <Text style={styles.dataTitle}>CURP:</Text>
-                                <Text style={styles.data}>{user.CURP}</Text>
-                            </View>
+                        </View>
                        
                     </View>
                 </View>
