89ee260574bb71f695cc41624bdf2e08a2f04c0e
BoJavs <javiereric1309@gmail.com>
2023-05-04 19:56:53 -0600

    Id.id

--- arco-system/src/Components/Acceso.jsx
+++ arco-system/src/Components/Acceso.jsx
@@ -57,8 +57,6 @@ const useStyles = makeStyles((theme) => ({
 
 function Acceso({isOpen, handleClose, user}) {
     const classes = useStyles();
-    console.log(user)
-
     const date = new Date();
     
     const handlePostRequest = () => {
